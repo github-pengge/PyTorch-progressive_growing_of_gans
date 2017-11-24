3144217383b5412b4aaf4fda43cb7e5e4a7d5164
Gapeng <hongjiapeng@hongjiapengdeMacBook-Pro.local>
2017-11-24 10:57:11 +0800

    read data from hdf5

--- utils/data.py
+++ utils/data.py
@@ -2,6 +2,7 @@
 import os, scipy.misc
 from glob import glob
 import numpy as np 
+import h5py
 
 
 prefix = './datasets/'
@@ -22,19 +23,47 @@ def get_img(img_path, is_crop=True, crop_h=256, resize_h=64, normalize=False):
     return np.transpose(cropped_image, [2, 0, 1])
 
 
+# class CelebA():
+#     def __init__(self):
+#         datapath = os.path.join(prefix, 'celeba/aligned')
+#         self.channel = 3
+#         self.data = glob(os.path.join(datapath, '*.jpg'))
+
+#     def __call__(self, batch_size, size):
+#         batch_number = len(self.data)/batch_size
+#         path_list = [self.data[i] for i in np.random.randint(len(self.data), size=batch_size)]
+#         file_list = [p.split('/')[-1] for p in path_list]
+#         batch = [get_img(img_path, True, 178, size, True) for img_path in path_list]
+#         batch_imgs = np.array(batch).astype(np.float32)
+#         return batch_imgs
+
+#     def save_imgs(self, samples, file_name):
+#         N_samples, channel, height, width = samples.shape
+#         N_row = N_col = int(np.ceil(N_samples**0.5))
+#         combined_imgs = np.ones((channel, N_row*height, N_col*width))
+#         for i in range(N_row):
+#             for j in range(N_col):
+#                 if i*N_col+j < samples.shape[0]:
+#                     combined_imgs[:,i*height:(i+1)*height, j*width:(j+1)*width] = samples[i*N_col+j]
+#         combined_imgs = np.transpose(combined_imgs, [1, 2, 0])
+#         scipy.misc.imsave(file_name+'.png', combined_imgs)
+
+
 class CelebA():
     def __init__(self):
-        datapath = os.path.join(prefix, 'celeba/aligned')
-        self.channel = 3
-        self.data = glob(os.path.join(datapath, '*.jpg'))
+        datapath = 'celeba-hq-1024x1024.h5'
+        resolution = ['data2x2', 'data4x4', 'data8x8', 'data16x16', 'data32x32', 'data64x64', \
+                        'data128x128', 'data256x256', 'data512x512', 'data1024x1024']
+        self._base_key = 'data'
+        self.dataset = h5py.File(os.path.join(prefix, datapath), 'r')
+        self._len = {len(self.dataset[k]) for k in self.dataset}
+        assert all([resol in self.dataset.keys() for resol in resolution])
 
     def __call__(self, batch_size, size):
-        batch_number = len(self.data)/batch_size
-        path_list = [self.data[i] for i in np.random.randint(len(self.data), size=batch_size)]
-        file_list = [p.split('/')[-1] for p in path_list]
-        batch = [get_img(img_path, True, 178, size, True) for img_path in path_list]
-        batch_imgs = np.array(batch).astype(np.float32)
-        return batch_imgs
+        key = self._base_key + '{}x{}'.format(size, size)
+        idx = np.random.randint(self._len[key], size=batch_size)
+        batch_x = np.array([self.dataset[key][i]/127.5-1.0 for i in idx], dtype=np.float32)
+        return batch_x
 
     def save_imgs(self, samples, file_name):
         N_samples, channel, height, width = samples.shape
